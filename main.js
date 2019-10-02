a80d1947861eb6a6c3f39a8cf402668b9c080fb7
Ethan Shen <shenye601@hotmail.com>
2019-10-02 21:05:11 +1000

    js essential

---
+++ main.js
@@ -0,0 +1,31 @@
+// console.log('JS Essential');
+//var is globally scoped
+//let is mutable
+//const: have to be initialized and cannot be re-assigned
+// let age;
+// age = 30;
+// age = 20;
+// const score = 100;
+// console.log(age)
+// console.log(score);
+
+//String, numbers, Boolean, null, undefined
+const name = 'ethn';
+const age = 40;
+const isCool = true;
+const rating = 4.5;
+const x = null;
+const y = undefined;
+let z;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof z);
+console.log(typeof y);
+// string concatenation
+console.log(`My name is ${name} and I am ${age}`);
+const s = 'Hello World'
+console.log(s.substring(0 ,5).toUpperCase());
+// Arrays - variables that hold multiple values
+const number = new Array(1, 2, 3, 4, 5);
+console.log(number);
